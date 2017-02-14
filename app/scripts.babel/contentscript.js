3ed59fdd0f76098925ade1b8c07445258af8084e
Tomohiro Murota <tomohiro.murota@gmail.com>
2017-02-14 23:32:12 +0900

    Implement content script

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+const bindings = document.querySelectorAll('[data-team-name]');
+if (bindings && bindings.length > 0) {
+  // Get attribute value of "[data-team-name]".
+  const teamName = bindings[0].dataset.teamName;
+  const bgColor = localStorage.getItem('bgcolor:' + teamName);
+  // Switch header bg color.
+  const headers = document.getElementsByTagName('header');
+  if (bgColor && headers && headers.length > 0) {
+    headers[0].style.backgroundColor = bgColor;
+  }
+}
