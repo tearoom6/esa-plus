63d203229117e8374b6abf8eb2bbc54b4e3c57a4
Tomohiro Murota <tomohiro.murota@gmail.com>
2017-02-16 02:27:24 +0900

    Add browser action & Fix content script

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,13 +1,28 @@
-'use strict';
+'use strict'
 
-const bindings = document.querySelectorAll('[data-team-name]');
+/*
+ * Switch header bg color.
+ */
+const setTeamColor = (teamName) => {
+  chrome.storage.sync.get('teams', (items) => {
+    const headers = document.getElementsByTagName('header')
+    if (! items || ! items.teams || ! headers || headers.length <= 0) {
+      return
+    }
+    const teams = JSON.parse(items.teams)
+    for (const team of teams) {
+      if (team.teamName == teamName) {
+        headers[0].style.backgroundColor = team.teamColor
+        break
+      }
+    }
+	})
+}
+
+const bindings = document.querySelectorAll('[data-team-name]')
 if (bindings && bindings.length > 0) {
   // Get attribute value of "[data-team-name]".
-  const teamName = bindings[0].dataset.teamName;
-  const bgColor = localStorage.getItem('bgcolor:' + teamName);
-  // Switch header bg color.
-  const headers = document.getElementsByTagName('header');
-  if (bgColor && headers && headers.length > 0) {
-    headers[0].style.backgroundColor = bgColor;
-  }
+  const teamName = bindings[0].dataset.teamName
+  setTeamColor(teamName)
 }
+
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,44 @@
-'use strict';
+'use strict'
 
-console.log('\'Allo \'Allo! Popup');
+const defaultColor = 'ffffff'
+const app = new Vue({
+  el: '#app',
+  data: {
+    locales: {
+      appTitle:   chrome.i18n.getMessage('appName'),
+      addTeam:    chrome.i18n.getMessage('button_addTeam'),
+      removeTeam: chrome.i18n.getMessage('button_removeTeam'),
+    },
+    teamName: '',
+    teamColor: defaultColor,
+    teams: [
+    ]
+  },
+  created: function() {
+    chrome.storage.sync.get('teams', (items) => {
+      if (items && items.teams) {
+        this.teams = JSON.parse(items.teams)
+      }
+    })
+  },
+  methods: {
+    addTeam: function() {
+      let teamName = this.teamName.trim()
+      let teamColor = this.teamColor.trim()
+      if (teamName && teamColor) {
+        this.teams.push({
+          teamName: teamName,
+          teamColor: `#${teamColor}`,
+        })
+        chrome.storage.sync.set({'teams': JSON.stringify(this.teams)})
+
+        this.teamName = ''
+        this.teamColor = defaultColor
+      }
+    },
+    removeTeam: function(index) {
+      this.teams.splice(index, 1)
+      chrome.storage.sync.set({'teams': JSON.stringify(this.teams)})
+    }
+  }
+})
